3894ada1628174113be0e78ecf4730ec32d36390
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-05-22 15:38:57 +0200

    basic vite-vue structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import appHeader from "./components/appHeader.vue";
+import mainBox from "./components/mainBox.vue";
+
+export default {
+  components: {
+    appHeader,
+    mainBox,
+  },
+};
+</script>
+
+<template>
+  <appHeader />
+  <maniBox />
+</template>
+
+<style lang="scss">
+@use "../src/assets/style/general.scss";
+</style>
